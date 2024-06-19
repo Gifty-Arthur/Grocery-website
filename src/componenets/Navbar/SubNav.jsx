788393c4581d15f646c88b6aef03dd7220310eca
Gifty Arthur <giftyarthur031@gmail.com>
2024-06-19 21:29:53 +0000

    update

--- src/componenets/Navbar/SubNav.jsx
+++ src/componenets/Navbar/SubNav.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { PiPhoneCallBold } from "react-icons/pi";
+import { BiChevronDown } from "react-icons/bi";
 
 function SubNav() {
   return (
@@ -7,42 +8,220 @@ function SubNav() {
       <div className="max-w-7xl ">
         <div className="flex justify-between items-center py-4">
           <nav className="hidden md:flex flex-row items-center text-lg font-medium gap-8">
-            <Link
-              to="home"
-              spy={true}
-              smooth={true}
-              duration={500}
-              className="cursor-pointer hover:text-primary transition-all"
-            >
-              Home
-            </Link>
-            <Link
-              to="shop"
-              spy={true}
-              smooth={true}
-              duration={500}
-              className="cursor-pointer hover:text-primary transition-all"
-            >
-              Shop
-            </Link>
-            <Link
-              to="blog"
-              spy={true}
-              smooth={true}
-              duration={500}
-              className="cursor-pointer hover:text-primary transition-all"
-            >
-              Blog
-            </Link>
-            <Link
-              to="about"
-              spy={true}
-              smooth={true}
-              duration={500}
-              className="cursor-pointer hover:text-primary transition-all"
-            >
-              About Us
-            </Link>
+            <div className="relative group">
+              <div className=" flex items-center gap-1">
+                <Link
+                  to="dishes"
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  className="hover:text-primary transition-all cursor-pointer"
+                >
+                  Home
+                </Link>
+
+                <BiChevronDown
+                  className="cursor-pointer text-primary"
+                  size={25}
+                />
+              </div>
+
+              <ul className="absolute hidden space-y-2 group-hover:block bg-white border border-gray-300 rounded-lg p-5">
+                <li>
+                  <Link
+                    to="dishes"
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                    className="text-gray-800 hover:text-primary transition-all cursor-pointer"
+                  >
+                    Product
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="dishes"
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                    className="text-gray-800 hover:text-primary transition-all cursor-pointer"
+                  >
+                    Categories
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="dishes"
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                    className="text-gray-800 hover:text-primary transition-all cursor-pointer"
+                  >
+                    Sellers
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="dishes"
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                    className="text-gray-800 hover:text-primary transition-all cursor-pointer"
+                  >
+                    Latest
+                  </Link>
+                </li>
+              </ul>
+            </div>
+
+            {/* shop */}
+            <div className="relative group">
+              <div className=" flex items-center gap-1">
+                <Link
+                  to="dishes"
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  className="hover:text-primary transition-all cursor-pointer"
+                >
+                  Shop
+                </Link>
+
+                <BiChevronDown
+                  className="cursor-pointer text-primary"
+                  size={25}
+                />
+              </div>
+
+              <ul className="absolute hidden space-y-2 group-hover:block bg-white border border-gray-300 rounded-lg p-5">
+                <li>
+                  <Link
+                    to="dishes"
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                    className="text-gray-800 hover:text-primary transition-all cursor-pointer"
+                  >
+                    Sales
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="dishes"
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                    className="text-gray-800 hover:text-primary transition-all cursor-pointer"
+                  >
+                    Categories
+                  </Link>
+                </li>
+              </ul>
+            </div>
+
+            {/* blog */}
+            <div className="relative group">
+              <div className=" flex items-center gap-1">
+                <Link
+                  to="dishes"
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  className="hover:text-primary transition-all cursor-pointer"
+                >
+                  Blog
+                </Link>
+
+                <BiChevronDown
+                  className="cursor-pointer text-primary"
+                  size={25}
+                />
+              </div>
+
+              <ul className="absolute hidden space-y-2 group-hover:block bg-white border border-gray-300 rounded-lg p-5">
+                <li>
+                  <Link
+                    to="dishes"
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                    className="text-gray-800 hover:text-primary transition-all cursor-pointer"
+                  >
+                    Fresh Fruit
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="dishes"
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                    className="text-gray-800 hover:text-primary transition-all cursor-pointer"
+                  >
+                    Comments
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="dishes"
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                    className="text-gray-800 hover:text-primary transition-all cursor-pointer"
+                  >
+                    New Items
+                  </Link>
+                </li>
+                <li></li>
+              </ul>
+            </div>
+
+            {/* Abouts */}
+
+            <div className="relative group">
+              <div className=" flex items-center gap-1">
+                <Link
+                  to="dishes"
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  className="hover:text-primary transition-all cursor-pointer"
+                >
+                  About Us
+                </Link>
+
+                <BiChevronDown
+                  className="cursor-pointer text-primary"
+                  size={25}
+                />
+              </div>
+
+              <ul className="absolute hidden space-y-2 group-hover:block bg-white border border-gray-300 rounded-lg p-5">
+                <li>
+                  <Link
+                    to="dishes"
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                    className="text-gray-800 hover:text-primary transition-all cursor-pointer"
+                  >
+                    OurTeam
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="dishes"
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                    className="text-gray-800 hover:text-primary transition-all cursor-pointer"
+                  >
+                    OrganicFood
+                  </Link>
+                </li>
+              </ul>
+            </div>
+
             <Link
               to="contact"
               spy={true}
@@ -61,6 +240,7 @@ function SubNav() {
             </p>
           </div>
         </div>
+        {/* menu */}
       </div>
     </div>
   );
