32df1003c8db01f45d46d59c3ec131ad47fd6440
Gifty Arthur <giftyarthur031@gmail.com>
2024-06-22 03:53:49 +0000

    update

--- src/componenets/Home/Numbers.jsx
+++ src/componenets/Home/Numbers.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 /* eslint-disable no-undef */
 import CountUp from "react-countup";
 import React, { useState } from "react";
@@ -22,7 +24,7 @@ function Numbers({ counterOn, setCounterOn }) {
       onEnter={() => setCounterOn(true)}
       onExit={() => setCounterOn(false)}
     >
-      <div className="relative hidden  sm:flex md:flex lg:flex">
+      <div className="relative mt-20 hidden  sm:flex md:flex lg:flex">
         <img className="w-full md:mt-0 mt-20" src={num1} alt="" />
         <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center ">
           {/* one */}6
@@ -87,6 +89,91 @@ function Numbers({ counterOn, setCounterOn }) {
                   )}
                 </h1>
                 <p className="text-md text-white">Years of Hard work</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* mobile */}
+      <div className="flex sm:hidden bg-[#001109] mt-10">
+        <div className="container mt-10">
+          <div className=" flex flex-row gap-8">
+            {/* mone */}
+            <div className="w-[150px] h-[174px] bg-[#121d18]">
+              <div className="flex items-center justify-center flex-col mt-8">
+                <h1 className="text-[29px] text-primary mt-10">
+                  {counterOn && (
+                    <CountUp
+                      start={0}
+                      end={37}
+                      duration={3}
+                      delay={0}
+                      suffix="K+"
+                    />
+                  )}
+                </h1>
+                <p className="text-md text-white">Years of Hard work</p>
+              </div>
+            </div>
+            {/* mtwo */}
+            <div className="w-[150px] h-[174px] bg-[#121d18]">
+              <div className="flex items-center justify-center flex-col mt-8">
+                <h1 className="text-[29px] text-primary mt-10">
+                  {counterOn && (
+                    <CountUp
+                      start={0}
+                      end={500}
+                      duration={3}
+                      delay={0}
+                      suffix="K"
+                    />
+                  )}
+                </h1>
+                <p className="text-md text-white">Happy Customer</p>
+              </div>
+            </div>
+          </div>
+
+          {/* three */}
+          <div className="flex flex-row gap-6  mt-8">
+            <div className="w-[150px] h-[174px] bg-[#121d18]">
+              <div className="flex items-center justify-center flex-col mt-8">
+                <h1 className="text-[29px] text-primary mt-10">
+                  {counterOn && (
+                    <CountUp
+                      start={0}
+                      end={28}
+                      duration={3}
+                      delay={0}
+                      suffix=""
+                    />
+                  )}
+                </h1>
+
+                <p className="text-md text-white text-center">
+                  {" "}
+                  Team Member Qualified{" "}
+                </p>
+              </div>
+
+              {/* fourth */}
+            </div>
+
+            <div className="w-[150px] h-[174px] bg-[#121d18]">
+              <div className="flex items-center justify-center flex-col mt-8">
+                <h1 className="text-[29px] text-primary mt-10">
+                  {counterOn && (
+                    <CountUp
+                      start={0}
+                      end={750}
+                      duration={3}
+                      delay={0}
+                      suffix="K+"
+                    />
+                  )}
+                </h1>
+                <p className="text-md text-white">Monthly Orders</p>
               </div>
             </div>
           </div>
--- src/componenets/Home/SubCat.jsx
+++ src/componenets/Home/SubCat.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import Button from "../Layout/Button";
 
 // Images
-import bgcat from "../../assets/categories/bgcat.png";
+
 import sbcat1 from "../../assets/categories/sbcat1.png";
 import sbcat2 from "../../assets/categories/sbcat2.png";
 
@@ -13,81 +13,51 @@ import Numbers from "./Numbers";
 
 function SubCategories() {
   return (
-    <div>
-      <div>
-        <div className="relative">
-          <img src={bgcat} alt="" className="w-full  md:h-auto " />
-          <div className=" absolute top-0 left-0 w-full text-center text-white p-20 mt-20">
-            <div className="container">
-              <div className="md:flex md:flex-row md:gap-6">
-                <div className="flex flex-col">
-                  <div className="md:flex md:flex-row md:gap-6">
-                    <img
-                      className="md:w-[276px] md:h-[400px] w-full  h-full"
-                      src={sbcat2}
-                      alt=""
-                    />
-                    <img
-                      className="hidden  sm:flex md:flex lg:flex "
-                      src={sbcat1}
-                      alt=""
-                    />
-                    <div className="flex flex-col md:items-start md:ml-8">
-                      <h1 className="font-bold text-custom1 md:text-5xl text-lg md:mt-0 mt-4 font-poppins">
-                        100% Trusted
-                      </h1>
-                      <h1 className="font-bold text-custom1 md:text-5xl text-lg font-poppins">
-                        Organic Food Store
-                      </h1>
-                      {/* health */}
-                      <div>
-                        <div className="mt-6 flex flex-row gap-2 ">
-                          <IoIosCheckmarkCircle
-                            size={25}
-                            className="text-primary"
-                          />
-                          <h1 className="text-custom1 font-bold md:font-medium md:text-[18px] text-sm">
-                            Healthy & natural food for lovers of healthy food.
-                          </h1>
-                        </div>
-                        <p className="text-custom1 text-sm md:ml-6">
-                          quis tempus erat. Phasellus euismod bibendum magna non
-                          tristique. Pellentesque{" "}
-                          <span className="md:mr-4">
-                            semper vestibulum elit sed condimentum. Nunc pretium
-                            fermentum interdum.
-                          </span>
-                        </p>
-                        <div className="">
-                          <div className="mt-6 flex flex-row gap-2 hidden  sm:flex md:flex lg: ">
-                            <IoIosCheckmarkCircle
-                              size={25}
-                              className="text-primary"
-                            />
-                            <h1 className="text-custom1 font-medium text-[18px] hidden  sm:flex md:flex lg:flex">
-                              Every day fresh and quality products for you.
-                            </h1>
-                          </div>
-                          <p className="text-custom1 text-sm ml-6 hidden  sm:flex md:flex lg:flex">
-                            Maecenas vehicula a justo quis laoreet. Sed in
-                            placerat nibh, a posuere ex. Morbi sem neque,
-                            aliquam
-                          </p>
-                        </div>
-                        <Link>
-                          <Button title="Shop Now" Icon={IoArrowForward} />
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+    <div className="bg-[#f4f6f4] w-full">
+      <div className="container mt-20">
+        <div className="flex flex-col md:flex-row gap-8">
+          <div className="flex flex-col md:flex-row gap-8">
+            <img src={sbcat2} alt="" className="w-full md:w-auto" />
+            <img src={sbcat1} alt="" className="w-full md:w-auto" />
+          </div>
+
+          <div className="mt-8 md:mt-0 md:ml-20">
+            <h1 className="font-bold text-custom1 text-3xl md:text-5xl mt-4 font-poppins">
+              100% Trusted
+            </h1>
+            <h1 className="font-bold text-custom1 text-3xl md:text-5xl mt-4 font-poppins">
+              Organic Food Store
+            </h1>
+            <div className="mt-6 flex flex-col md:flex-row gap-2">
+              <IoIosCheckmarkCircle size={25} className="text-primary" />
+              <h1 className="text-custom1 font-bold md:text-[18px] text-sm">
+                Healthy & natural food for lovers of healthy food.
+              </h1>
             </div>
+            <p className="te">
+              Ut quis tempus erat. Phasellus euismod bibendum magna non
+            </p>
+
+            {/* second */}
+            <div className="mt-6 flex flex-col md:flex-row gap-2">
+              <IoIosCheckmarkCircle size={25} className="text-primary" />
+              <h1 className="text-custom1 font-bold md:text-[18px] text-sm">
+                Every day fresh and quality products for you.
+              </h1>
+            </div>
+            <p className="te">
+              Ut quis tempus erat. Phasellus euismod bibendum magna non
+            </p>
+
+            {/* Button */}
+            <Link>
+              <Button title="Shop Now" Icon={IoArrowForward} />
+            </Link>
           </div>
         </div>
-        {/* dark */}
-        <Numbers />
       </div>
+      {/* dark */}
+      <Numbers />
     </div>
   );
 }
